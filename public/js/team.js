ba1737f39858bd6691c341cbebf98ab07b001511
boyism80 <boyism80@gmail.com>
2020-09-12 01:33:48 +0900

    특정 팀 선택시 나타나는 페이지 데모

---
+++ public/js/team.js
@@ -0,0 +1,149 @@
+new Vue({
+    el: '#sections',
+    data: {
+        sections: [
+            {
+                title: 'Lorem ipsum',
+                todo_list: [
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    },
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    },
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    },
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    }
+                ]
+            },
+            {
+                title: 'Lorem ipsum',
+                todo_list: [
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    },
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    }
+                ]
+            },
+            {
+                title: 'Lorem ipsum',
+                todo_list: [
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    },
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    }
+                ]
+            },
+            {
+                title: 'Lorem ipsum',
+                todo_list: [
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    },
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    }
+                ]
+            },
+            {
+                title: 'Lorem ipsum',
+                todo_list: [
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    },
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    }
+                ]
+            },
+            {
+                title: 'Lorem ipsum',
+                todo_list: [
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    },
+                    {
+                        title: 'Lorem ipsum',
+                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                        owner: {
+                            name: 'Tyrion Lannister',
+                            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+})
