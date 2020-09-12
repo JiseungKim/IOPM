90fa90f160167e0f1fd57f7eaa697b38edda7414
boyism80 <boyism80@gmail.com>
2020-09-12 18:09:08 +0900

    팀 멤버 페이지 프로토타입

---
+++ public/js/team.members.js
@@ -0,0 +1,91 @@
+new Vue({
+    el: '#contents',
+    data: {
+        team: {
+            name: 'IOPM',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+        },
+        members: [
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            },
+            {
+                name: 'Tyrion Lannister',
+                email: 'boyism80@gmail.com'
+            }
+        ]
+    },
+})
