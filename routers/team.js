cbf57c505a22ca0d4b5caf95711d4487ef917eaa
boyism80 <boyism80@gmail.com>
2020-09-16 20:24:34 +0900

    team 라우터 날려먹은거 복구

---
+++ routers/team.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+const router = Router()
+const async_handler = require('express-async-handler')
+const Team = require('../models/team')
+
+const team = new Team()
+
+router.get('/', async_handler(async (req, res, next) => {
+    res.render('teams')
+}))
+
+router.get('/:name', async_handler(async (req, res, next) => {
+    res.render('team')
+}))
+
+router.get('/:name/settings', async_handler(async (req, res, next) => {
+    res.render('team_settings')
+}))
+
+module.exports = router
