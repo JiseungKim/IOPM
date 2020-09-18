3490cc5cc51ed98101790f01c888d48d9bbfb4df
Jiseung <wltmd0426@gmail.com>
2020-09-18 17:10:32 +0900

    indent 맞춤 & db dump

--- example/helpers/cdn.js
+++
@@ -1,7 +0,0 @@
-const appsettings = require('./config')
-
-module.exports = {
-    cdn: function (path) {
-        return `${appsettings.cdn.protocol}://${appsettings.cdn.host}:${appsettings.cdn.port}/${path}`
-    }
-}
--- example/helpers/config.js
+++
@@ -1,1 +0,0 @@
-module.exports = require(`../config/appsettings.${process.env.ENVIRONMENT || 'local'}.json`)
